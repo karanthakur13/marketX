964ab316ad4ec65482a63eec3f370b3d019e2664
karanthakur13 <89067270+karanthakur13@users.noreply.github.com>
2023-04-08 14:35:31 +0530

    listing products

--- contracts/Marketx.sol
+++ contracts/Marketx.sol
@@ -2,10 +2,45 @@
 pragma solidity ^0.8.9;
 
 contract Marketx {
-    string public name;
+
+    address public owner;
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
 
     constructor() {
-        name = "Marketx";
+        owner = msg.sender;
+    }
+
+    function list(uint256 _id,
+                string memory _name,
+                string memory _category,
+                string memory _image,
+                uint256 _cost,
+                uint256 _rating,
+                uint256 _stock ) public {
+
+        Item memory item = Item(_id,
+                _name,
+                _category,
+                _image,
+                _cost,
+                _rating,
+                _stock
+            );
+
+    items[_id] = item;
+ 
+
+
     }
 
 
--- test/Marketx.js
+++ test/Marketx.js
@@ -6,10 +6,45 @@ const tokens = (n) => {
 }
 
 describe("Marketx", () => {
-  it('has a name', async () => {
+  let marketx 
+  let deployer ,buyer 
+  
+
+  beforeEach(async () =>{
+
+    [deployer,buyer] = await ethers.getSigners()
+    /* console.log(deployer.address,buyer.address) */
     const Marketx = await ethers.getContractFactory("Marketx")
     marketx = await Marketx.deploy()
-    const name = await marketx.name()
-    expect (name).to.equal("Marketx")
-  })  
+  })
+
+  describe("Deployment", () => {
+
+    it('Sets the owner' ,async ()=>{
+      expect(await marketx.owner()).to.equal(deployer.address)
+    })
+  })
+
+  describe("Listing",() =>{
+    let transaction
+    let ID = 1
+
+    beforeEach(async() =>{
+      transaction = await marketx.connect(deployer).list(
+        1,
+        "shoes",
+        "clothing",
+        "ImAGE",
+        12,
+        4,
+        10)
+      await transaction.wait()
+    })
+
+    it("Returns item attributes",async()=>{
+      const item = await marketx.items(ID)
+      expect(item.id).to.equal(ID)
+    })
+  })
+  
 })
